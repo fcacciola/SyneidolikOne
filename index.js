0fba10055f62c107eca6cabfe805cb2ae3658e34
Fernando Cacciola <fernando.cacciola@gmail.com>
2022-10-14 23:24:14 -0300

    updates

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 gIdx=0 ;
-cSize=3 ;
+cSize=5 ;
 cTotal=cSize*cSize;
+
+function resetInfo()
+{
+  document.getElementById("info").innerHTML = "";  
+}
 
 function setInfo(text)
 {
@@ -10,6 +15,24 @@ function setInfo(text)
 function addInfo(text)
 {
   document.getElementById("info").innerHTML += text + "\n";  
+}
+
+function help_0()
+{
+  setInfo
+  (
+  `
+When learnig how to use this, first click on Show. You will see two 5x5 tiles. 
+The tile on the Right has a computer generated black and white pattern.
+You can click on Reset to regenerate the pattern (and you can click on Show again to see it).
+Your goal is to match the computer generated pattern WITHOUT seeing it, by guessing each cell color (black or white), one at a time.
+For each of the nine cells, you will be given a secretly hidden message that should tell you, subliminally,the correct color of the corresponding cell.
+After all 25 cells have been choosen (right or wrong), your pattern will be shown on the left, and the computer generated pattern on the right.
+You will also be given a Score based on how many cells you guessed correctly.
+Click on Start to give it a try. When finished, you can click on Start again to repeat it.
+GOOD LUCK!
+  `
+  )
 }
 
 function get_cell(g,r,c)
@@ -53,7 +76,6 @@ function set_cell_color_by_idx(g,gIdx,clr)
 {
   r = Math.floor(gIdx / cSize);
   c = gIdx - ( r * cSize ) ;
-  //addInfo("gIdx=" + gIdx + " r=" + r + " c=" + c ) ;
   set_cell_color(g,r,c,clr);
   show_cell(g,r,c);
 }
@@ -74,8 +96,8 @@ colors = { 'a' : [ "cyan", "cyan" ,"cyan", "cyan" ,"cyan",
 
 function setup_grid(g)
 {
-  lx = g == 'a' ? 25 : 200 ;
-  ly = 100 ;
+  lx = g == 'a' ? 25 : 300 ;
+  ly = 120 ;
 
   for( r = 0 ; r < cSize ; ++ r ) 
   {
@@ -135,10 +157,7 @@ function setup_random_grid()
   {
     for ( c = 0 ; c < cSize ; ++ c )
     {
-      rnd = Math.random();
-      irnd = Math.floor(rnd * 1000) ;
-      even = ( irnd % 2 ) == 0 ;
-      color = ( even ? "white" : "black" ) ;
+      color = ( random_even_odd() ? "white" : "black" ) ;
   
       set_cell_color('b', r, c, color);
       colors['b'][(r*cSize)+c] = color ;
@@ -190,16 +209,16 @@ function set_micro_image_pixel_color_random( ctx, x, y)
 function setup_micro_image()
 {
   const canvas = document.getElementById('micro_image');
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d', {willReadFrequently: true});
 
-  for( y = 0 ; y < 9 ; ++ y ) 
+  for( y = 0 ; y < 15 ; ++ y ) 
   {
-    for ( x = 0 ; x < 9 ; ++ x )
+    for ( x = 0 ; x < 15 ; ++ x )
     {
       in_cell = false ; 
-      if ( x >= 0 && x < 3 )
+      if ( x >= 0 && x < 5 )
       {
-        if ( y >= 0 && y < 3 )
+        if ( y >= 0 && y < 5 )
         {
           if ( is_cell_white(y,x) )
                set_micro_image_pixel_color_white(ctx,x,y) ;
@@ -214,27 +233,39 @@ function setup_micro_image()
   }
 }
 
+function clear_hint_image()
+{
+  const canvas = document.getElementById('aca');
+  const ctx = canvas.getContext('2d');  
+  
+  for( y = 0 ; y < 400 ; ++ y ) 
+  {
+    for ( x = 0 ; x < 400 ; ++ x )
+    {
+      set_micro_image_pixel_color_white(ctx,x,y) ;   
+    }
+  }
+
+}
+
+function random_even_odd()
+{
+  rnd = Math.random();
+  irnd = Math.floor(rnd * 1000) ;
+  even = ( irnd % 2 ) == 0 ;
+  return even ;
+}
+
 function setup_hint_image()
 {
   const canvas = document.getElementById('aca');
   const ctx = canvas.getContext('2d');  
   
-  const img = new Image();
-  img.crossOrigin = 'anonymous';
-  img.src = './assets/4rai.png';  
-  img.addEventListener('load', () => {
-    ctx.drawImage(img, 0, 0);
-    //img.style.display = 'none';
-  });
-
   for( y = 0 ; y < 400 ; ++ y ) 
   {
     for ( x = 0 ; x < 400 ; ++ x )
     {
-      rnd = Math.random();
-      irnd = Math.floor(rnd * 1000) ;
-      even = ( irnd % 2 ) == 0 ;
-      if ( even )
+      if ( random_even_odd() )
            set_micro_image_pixel_color_white(ctx,x,y) ;   
       else set_micro_image_pixel_color_black(ctx,x,y) ;
     }
@@ -264,7 +295,7 @@ function calculate_score()
 
 function show_score(score)
 {
-  document.getElementById("score").innerHTML = "Score: " + score;  
+  document.getElementById("score").innerHTML = `Score: ${score} points (+1/-1 for each correct/incorrect color).`;  
 }
 
 function set_next_cell_color(color)
@@ -275,7 +306,7 @@ function set_next_cell_color(color)
 
     ++ gIdx ;
     if ( gIdx < cTotal )
-      set_cell_color_by_idx('a',gIdx,"gray");
+      ready_for_next_choice();
   }
 
   if ( gIdx >= cTotal )
@@ -296,21 +327,73 @@ function set_next_cell_as_black()
   set_next_cell_color("black");
 }
 
-function start_choosing()
+function reset()
 {
   gIdx = 0 ;
   setup_random_grid()
   hide_grids();
-  set_cell_color_by_idx('a',0,"gray");
+  clear_hint_image();
+  show_score(0);
+}
+
+function ready_for_next_choice()
+{
+  if ( gIdx > 0 )
+    set_cell_color_by_idx('a',gIdx-1,"LightGray");
+  set_cell_color_by_idx('a',gIdx,"gray");
+  setup_hint_image();
+
+  even = random_even_odd(); 
+  color1 = even ? 'WHITE' : 'BLACK' ;
+  color2 = even ? 'BLACK' : 'WHITE' ;
+
+  r = Math.floor(gIdx / cSize);
+  c = gIdx - ( r * cSize ) ;
+
+  setInfo
+  (
+  `
+Now you have to guess whether the (hidden) computer generated color at row ${r+1} and column ${c+1},
+shown above as a drak-gray cell, is either black or white.
+   
+In order to figure that out, imagine that there is an invisible person sitting right next to you.
+He knows the correct color and is telling you which is it. But secretly, so that no one else knows it too.
+
+That (imaginary) invisible person is telling you the correct color by hiding a non-written, symbolic message in the fuzzy image shown above. 
+
+Imagine the hidden message somehow saying, BUT WIHTOUT USING WORDS, that the correct color is either ${color1} or ${color2}.
+Try as hard as you can to discover the hidden message in the image. 
+  
+TAKE YOUR TIME.
+  
+Once you FEEL that you've got the correct answer, click on the large ${color2} or ${color1} box.
+The program will move to the next cell after each color selection, until your have guessed all 25 cells.
+At the end, the hidden pattern will be revealed for you to compare, and a score will be calculated.
+  `
+  )
+
+}
+
+function start_choosing()
+{
+  reset();  
+  gIdx = 0 ;
+  ready_for_next_choice();
 }
 
 function init()
 {
   setup_grids();
-  setup_random_grid()
-  hide_grids();
+  reset();
+  
+  setInfo
+  (
+  `
+  Click on FIRST TIME INSTRUCTIONS if you have never used this before.
+  Click on Start if you already know what to do.
+  `
+  )
 
-  setup_hint_image();
 }
 
 init();
